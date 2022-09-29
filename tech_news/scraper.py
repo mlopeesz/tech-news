c93ef2a0c1665ab0d390ea6fad771d3c28ea536e
Mauricio Lopes <mlopeesz@gmail.com>
2022-09-29 11:43:03 -0300

    feat: :sparkles: scrape_noticia function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -37,8 +37,21 @@ def scrape_next_page_link(html_content: str):
 
 
 # Requisito 4
-def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+def scrape_noticia(html_content: str):
+    selector = Selector(text=html_content)
+    result = {
+        "url": selector.css('link[rel="canonical"]::attr(href)').get(),
+        "title": selector.css(".entry-title::text").get().strip(),
+        "timestamp": selector.css(".meta-date ::text").get(),
+        "writer": selector.css(".author a::text").get(),
+        "comments_count": len(selector.css("div.comment-body").getall()),
+        "summary": "".join(
+            selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
+        ).strip(),
+        "tags": selector.css("[rel=tag]::text").getall(),
+        "category": selector.css(".label ::text").get(),
+    }
+    return result
 
 
 # Requisito 5
